b6ae529fd9022015dfaae7024052f57c5faaab11
prazega95 <prazega95@gmail.com>
2020-05-03 15:19:59 -0500

    Modificando clases sintomas 1 y 2

--- COVID19ASISTENCIA/app/src/main/java/com/example/prado/covid19_asistencia/sintomas1.java
+++ COVID19ASISTENCIA/app/src/main/java/com/example/prado/covid19_asistencia/sintomas1.java
@@ -1,13 +1,42 @@
 package com.example.prado.covid19_asistencia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class sintomas1 extends AppCompatActivity {
+public class sintomas1 extends AppCompatActivity implements View.OnClickListener{
+
+
+    Button btn1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sintomas1);
+
+
+        btn1 =(Button) findViewById(R.id.btnSiguienteSintoma1);
+        btn1.setOnClickListener(this);
+
     }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.btnSiguienteSintoma1:
+                Intent int1= new Intent(this, sintomas2.class);
+                startActivity(int1);
+                break;
+
+
+        }
+    }
+
+
 }
--- COVID19ASISTENCIA/app/src/main/java/com/example/prado/covid19_asistencia/sintomas2.java
+++ COVID19ASISTENCIA/app/src/main/java/com/example/prado/covid19_asistencia/sintomas2.java
@@ -1,13 +1,39 @@
 package com.example.prado.covid19_asistencia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class sintomas2 extends AppCompatActivity {
+public class sintomas2 extends AppCompatActivity  implements View.OnClickListener{
 
+
+
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sintomas2);
+
+        btn1 =(Button) findViewById(R.id.btnSiguienteSintoma2);
+        btn1.setOnClickListener(this);
+
+
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.btnSiguienteSintoma2:
+                Intent int1= new Intent(this, sintomas3.class);
+                startActivity(int1);
+                break;
+
+
+        }
+    }
+
+
 }
